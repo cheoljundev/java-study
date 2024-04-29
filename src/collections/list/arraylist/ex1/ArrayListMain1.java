b7e2f68c5fdb093a346ee62ae39cde8a88a227d5
cheoljundev <cheoljundev@gmail.com>
2024-04-29 10:52:15 +0900

    ArrayList

---
+++ src/collections/list/arraylist/ex1/ArrayListMain1.java
@@ -0,0 +1,41 @@
+package collections.list.arraylist.ex1;
+
+import java.util.ArrayList;
+
+public class ArrayListMain1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("JDBC");
+        list.add("Servlet/JSP"); // 값 추가
+        list.add(2, "Database"); // 해당 인덱스에 값 추가
+        list.add("iBATIS");
+
+        int size = list.size(); // 저장된 데이터 수 읽기
+        System.out.println("size = " + size);
+        System.out.println();
+
+        String skill = list.get(2); // 인덱스의 객체 찾기
+        System.out.println("2: " + skill);
+        System.out.println();
+
+        for (int i = 0; i < size; i++) {
+            String str = list.get(i);
+            System.out.println(i + ":" + str);
+        }
+        System.out.println();
+
+        list.remove(2);
+        list.remove(2);
+        list.remove("iBATIS");
+
+        size = list.size();
+
+        for (int i = 0; i < size; i++) {
+            String str = list.get(i);
+            System.out.println(i + ":" + str);
+        }
+        
+    }
+}
